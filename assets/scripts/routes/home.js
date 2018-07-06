189d3458b9e46acf668bc4de8337dd2ecfdae708
Alvar Hyvönen <zArubaru@users.noreply.github.com>
2018-07-06 10:35:44 +0300

    Removed bower and upgraded js file structure. Dist files added to gitignore.

---
+++ assets/scripts/routes/home.js
@@ -0,0 +1,10 @@
+// import $ from 'jquery';
+
+export default {
+  init() {
+    // JavaScript to be fired on the home page
+  },
+  finalize() {
+    // JavaScript to be fired on the home page, after the init JS
+  },
+};
